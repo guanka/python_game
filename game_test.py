0aee8dd016359f47839e92514884d4edcc4780e1
guanka <guanka@outlook.com>
2016-10-28 09:11:41 +0800

    Add the enemy's plane

--- game_test.py
+++ game_test.py
@@ -1,7 +1,7 @@
 import pygame
 from sys import exit
 from pygame.locals import *
-import _random
+import random
 
 # defination
 SCREEN_WIDTH = 800
@@ -62,6 +62,18 @@ class Player(pygame.sprite.Sprite):
             self.rect.right += self.speed
 
 
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, enemy_img, enemy_down_imgs, init_pos):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = enemy_img
+        self.rect = self.image.get_rect()
+        self.rect.topleft = init_pos
+        self.down_imgs = enemy_down_imgs
+        self.speed = 2
+        self.down_index = 0
+
+    def move(self):
+        self.rect.top += self.speed
 pygame.init()
 # set screen size
 screen = pygame.display.set_mode((800, 480))
@@ -84,21 +96,67 @@ plane_image = pygame.image.load(r'resources/image/shoot.png')
 bullet_rect = pygame.Rect(1004, 987, 9, 21)
 bullet_image = plane_image.subsurface(bullet_rect)
 
+# enemy img
+enemy1_rect = pygame.Rect(534, 612, 57, 43)
+enemy1_img = plane_image.subsurface(enemy1_rect)
+enemy1_down_imgs = []
+enemy1_down_imgs.append(plane_image.subsurface(pygame.Rect(267, 347, 57, 43)))
+enemy1_down_imgs.append(plane_image.subsurface(pygame.Rect(873, 697, 57, 43)))
+enemy1_down_imgs.append(plane_image.subsurface(pygame.Rect(267, 296, 57, 43)))
+enemy1_down_imgs.append(plane_image.subsurface(pygame.Rect(930, 697, 57, 43)))
+
+enemies1 = pygame.sprite.Group()
+
+enemies_down = pygame.sprite.Group()
+enemy_frequency = 0
 player = Player(plane_image, player_rect, player_pos)
 clock = pygame.time.Clock()
-
+bullet_time = 0
+score = 0
 running = True
 
 while running:
     # screen.fill(0)
     clock.tick(60)
+    if enemy_frequency % 50 == 0:
+        enemy1_pos = [random.randint(0, SCREEN_WIDTH - enemy1_rect.width), 0]
+        enemy1 = Enemy(enemy1_img, enemy1_down_imgs, enemy1_pos)
+        enemies1.add(enemy1)
+    enemy_frequency += 1
+    if enemy_frequency >= 100:
+        enemy_frequency = 0
 
+    for enemy in enemies1:
+        enemy.move()
+        if enemy.rect.top < 0:
+            enemies1.remove(enemy)
+    enemies1_down = pygame.sprite.groupcollide(enemies1, player.bullets, 1, 1)
+    for enemy_down in enemies1_down:
+        enemies_down.add(enemy_down)
+    for enemy_down in enemies_down:
+        if enemy_down.down_index == 0:
+            pass
+        if enemy_down.down_index > 7:
+            enemies_down.remove(enemy_down)
+            score += 1000
+            continue
+        a = int (enemy_down.down_index / 2)
+        print(a)
+        screen.blit(enemy_down.down_imgs[a], enemy_down.rect)
+        enemy_down.down_index += 1
     screen.fill(0)
     screen.blit(background, (0, 0))
+    player.bullets.draw(screen)
+    enemies1.draw(screen)
     player.img_index += 1
     player.img_index %= 2
+    bullet_time += 1
     screen.blit(player.image[player.img_index], player.rect)
     pygame.display.update()
+    for bullet in player.bullets:
+        bullet.move()
+        if bullet.rect.bottom < 0:
+            player.bullets.remove(bullet)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -113,6 +171,8 @@ while running:
     if key_pressed[K_d]:
         player.move_right()
     if key_pressed[K_SPACE]:
-        player.shoot(bullet_image)
+        if bullet_time > 8:
+            bullet_time = 0
+            player.shoot(bullet_image)
 pygame.quit()
 exit()
